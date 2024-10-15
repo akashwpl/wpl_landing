bc84ae2db73a0da0a5625ad4375005ff269299f8
Akash-Behara <behara.akash68@gmail.com>
2024-10-15 23:21:22 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ["react-editor-js"],
     exclude: ['primereact']
   }
 })
